d36fd631fd05fb7d07994099ed08d3d3ec3ee641
aeronje <aeronje@hotmail.com>
2025-07-10 22:17:35 +0800

    changed to multi-line comment
    changed to multi-line comment

--- megaplanet/ntc/mobile_sim_registration_create.sql
+++ megaplanet/ntc/mobile_sim_registration_create.sql
@@ -4,7 +4,9 @@ Please note that the BAGONGPILIPINAS schema is not included in this version.
 If you are using IBM DB2 instead, refer to the 'databases_tables_triggers.md' in the repository for schema-specific setup.
 */
 
---Ron Penones | July 10th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+/*
+Ron Penones | July 10th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+*/
 
 create table mobile_sim_registration (mobile_sim_number VARCHAR (11) not null,
 mobile_sim_first_name VARCHAR (50) not null,
