d076144453ee48ebe83cdd173894c7288f36bf9f
aeronje <aeronje@hotmail.com>
2025-07-10 22:13:08 +0800

    changed to multi-line comment
    changed to multi-line comment

--- megaplanet/bir/tax_background_check_create.sql
+++ megaplanet/bir/tax_background_check_create.sql
@@ -4,6 +4,12 @@ Please note that the BAGONGPILIPINAS schema is not included in this version.
 If you are using IBM DB2 instead, refer to the 'databases_tables_triggers.md' in the repository for schema-specific setup.
 */
 
---Ron Penones | July 8th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+/*
+Ron Penones | July 8th 2025 - Feel free to share and reproduce, the core idea is mine with some assistance of AI. Padayon!
+*/
 
-create table tax_background_check (tax_identification_number VARCHAR(15) NOT NULL, tax_last_fiscal_gross_php DECIMAL(15,2) NOT NULL, tax_first_name VARCHAR(50) NOT NULL, tax_last_name VARCHAR(50) NOT NULL, tax_rdo VARCHAR(10) NOT NULL);
+create table tax_background_check (tax_identification_number VARCHAR(15) not null,
+tax_last_fiscal_gross_php DECIMAL(15, 2) not null,
+tax_first_name VARCHAR(50) not null,
+tax_last_name VARCHAR(50) not null,
+tax_rdo VARCHAR(10) not null);
